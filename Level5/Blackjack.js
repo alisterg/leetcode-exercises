d5dbbc8f0e08af85ca35616ca08553cf0c2750e0
Alister <z@Alisters-MacBook-Pro.local>
2019-12-16 11:28:51 +1000

    Add blackjack solution

---
+++ Level5/Blackjack.js
@@ -0,0 +1,27 @@
+
+/*
+ * Number cards count as their face value (2 through 10). 
+ * Jack, Queen and King count as 10. An Ace can be counted as either 1 or 11.
+ *
+ * Return the highest score of the cards that is less than or equal to 21. 
+ * If there is no score less than or equal to 21 return the smallest score more than 21.
+ * 
+ * NOTE: No validation. I'm aiming for less code
+ */
+
+function scoreHand(cards) {
+
+    const score = cards.reduce((acc, curr) => isNaN(curr)
+        ? curr === 'A'
+            ? acc + 11
+            : acc + 10
+        : acc + +curr, 0);
+
+    for (let i = 0; i < cards.filter(x => x === 'A').length; i++) {
+        if (score <= 21) continue;
+
+        score -= 10;
+    }
+
+    return score;
+}
