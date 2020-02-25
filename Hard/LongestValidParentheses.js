62837d5dbe92e4e8bb59135474d76398b3f26ac9
Alister <alister7@gmail.com>
2020-02-25 14:07:45 +1000

    Attempt longest parentheses

--- Hard/LongestValidParentheses.js
+++ Hard/LongestValidParentheses.js
@@ -6,27 +6,31 @@
  */
 var longestValidParentheses = function(parenthesesString) {
 
-    // Start calculating for a substring (i = 0)
-    // Adding opening braces is always valid
-    // If we have 1 too few OR 1 too many closing braces
-        // it is invalid, backtrack to the previous iteration
-    // find the valid count of the current substring, set it to the MAX if applicable
-    // When we have processed all substrings, return MAX
-
+    let openParenth = 0;
+    let currentPairs = 0;
+    let longestPair = 0;
+    
     for (let i = 0; i < parenthesesString.length; i++) {
-
-        if (parenthesesString[i] === '(') {
-            // Start another block of parentheses
-            //calculateSubstr(parenthesesBlock.substring(i + 1));
+     
+        if (parenthesesString[i] == '(') {
+            openParenth++; 
         }
-
-        if (parenthesesString[i] === ')') {
-            // Add this block of parentheses to the total
-
-            // Move on to the next block
+        else if (parenthesesString[i] == ')') {
+            if (openParenth > 0) {
+                openParenth--;
+                currentPairs++;
+            }
+            else {
+                if (currentPairs * 2 > longestPair) {
+                    longestPair = currentPairs * 2;
+                }
+    
+                currentPairs = 0;
+                openParenth = 0;
+            }
         }
-
     }
+
+    return longestPair;
 };
 
-()()(((())))))(
